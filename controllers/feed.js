f8c421ac19a1325fda8637ebbf2300bcc124d037
Dipoengoro <dipoengoro@outlook.com>
2023-07-28 14:28:12 +0700

    Initial commit

---
+++ controllers/feed.js
@@ -0,0 +1,25 @@
+exports.getPosts = (req, res) => {
+  res.status(200).json({
+    posts: [
+      {
+        title  : "First Post",
+        content: "This is the first post!"
+      }
+    ]
+  });
+};
+
+exports.postPost = (req, res) => {
+  const {
+    title,
+    content
+  } = req.body;
+  res.status(201).json({
+    message: "Post created successfully!",
+    post   : {
+      id     : new Date().toISOString(),
+      title  : title,
+      content: content
+    }
+  });
+};
